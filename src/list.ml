e080cfb951216159a287a8de7f755a7f8dcf9bd4
Jan Rochel <jrochel@besport.com>
2021-12-09 11:02:50 +0100

    List.TransPlus

--- src/list.ml
+++ src/list.ml
@@ -1,6 +1,27 @@
-module type MonadS = Monad.S
+module Trans (M : Monad.S) = struct
+  include Monad.Make (struct
+    type 'a m = 'a list M.m
 
-module MonadMake = Monad.Make
+    let return x = M.return [x]
+    let bind xs f = M.bind xs (fun x -> M.map BatList.concat (M.list_map f x))
+  end)
+
+  let lift x = M.map (fun x -> [x]) x
+end
+
+module TransPlus (M : MonadPlus.S) = struct
+  include MonadPlus.Make (struct
+    type 'a m = 'a list M.m
+
+    let return x = M.return [x]
+    let bind xs f = M.bind xs (fun x -> M.map BatList.concat (M.list_map f x))
+    let zero = M.zero
+    let plus = M.plus
+    let null = M.null
+  end)
+
+  let lift x = M.map (fun x -> [x]) x
+end
 
 module Monad = MonadPlus.Make (struct
   type 'a m = 'a list
@@ -11,14 +32,3 @@ module Monad = MonadPlus.Make (struct
   let plus xs ys = xs @ ys
   let null = function [] -> true | _ -> false
 end)
-
-module Trans (M : MonadS) = struct
-  include MonadMake (struct
-    type 'a m = 'a list M.m
-
-    let return x = M.return [x]
-    let bind xs f = M.bind xs (fun x -> M.map BatList.concat (M.list_map f x))
-  end)
-
-  let lift x = M.map (fun x -> [x]) x
-end
